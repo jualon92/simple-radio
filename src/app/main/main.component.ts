cdd65333df87808dde73ffc2569f24b6046ea0a7
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-15 01:52:18 -0300

    feat: add radio cards

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
+
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [ 
+    MatSlideToggleModule, 
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSliderModule
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
+export class MainComponent  {
 
 }
