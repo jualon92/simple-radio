0252199330901fa03847975034b054d44b74ab57
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-17 05:05:37 -0300

    feat: add radio frequencies

--- src/app/main/radios.ts
+++ src/app/main/radios.ts
@@ -1,16 +1,19 @@
 export const radioStations: any[] = [
   {
     name: 'Radio Rivadavia',
+    frequency: 'AM 630',
     url: 'https://playerservices.streamtheworld.com/api/livestream-redirect/RIVADAVIA.mp3',
     image: './assets/images/rivadavia.webp'
   },
   {
     name: 'Radio Continental',
+    frequency: 'AM 590',
     url: 'https://frontend.radiohdvivo.com/continental/live',
     image: './assets/images/continental.webp'
   },
   {
     name: 'Radio Mitre',
+    frequency: 'AM 790',
     url: 'https://playerservices.streamtheworld.com/api/livestream-redirect/AM790_56.mp3',
     image: './assets/images/mitre.webp'
   }
