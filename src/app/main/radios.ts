104e3c0f4919b9408966cc71404579527fd7a1da
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-19 21:54:19 -0300

    feat: add radios

--- src/app/main/radios.ts
+++ src/app/main/radios.ts
@@ -36,5 +36,68 @@ export const radioStations: Station[] = [
     isSelected: false,
     isLoading: false,
     isFooterStopped:false,
-  }
+  },
+  {
+    name: "Radio La 100",
+    frequency: "FM 99.9",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/FM999_56.mp3",
+    image: "./assets/images/la100.webp",
+    isSelected: false,
+    isLoading: false,
+    isFooterStopped:false,
+  },
+  {
+    name: "Radio Pop",
+    frequency: "FM 101.5",
+    url: "https://radiopop.radioca.st/stream",
+    image: "./assets/images/pop.webp",
+    isSelected: false,
+    isLoading: false,
+    isFooterStopped:false,
+
+  },{
+    name: "Radio Aspen",
+    frequency: "FM 102.3",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/ASPEN_SC",
+    image: "./assets/images/aspen.webp",
+    isSelected: false,
+    isLoading: false,
+    isFooterStopped:false,
+  },
+  {
+    name: "Radio Metro",
+    frequency: "FM 95.1",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/METRO.mp3",
+    image: "./assets/images/metro.webp",
+    isSelected: false,
+    isLoading: false,
+    isFooterStopped:false,
+  }/* , {
+    name: "Radio One",
+    frequency: "FM 103.7",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/ONE_SC",
+    image: "./assets/images/one.webp",
+    isSelected: false,
+    isLoading: false,
+    isFooterStopped:false,
+  }, *//* 
+  {
+    name: "Radio 10",
+    frequency: "AM 710",
+    url: "https://radio10.stweb.tv/radio10/live/l_70_240822023_37965.aac?nimblesessionid=709437582",
+    image: "./assets/images/radio10.webp",
+    isSelected: false,
+    isLoading: false,
+    isFooterStopped:false,
+  } */
+  /* {
+    name: "Radio Disney",
+    frequency: "FM 94.3",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/DISNEY_SC",
+    image: "./assets/images/disney.webp",
+    isSelected: false,
+    isLoading: false,
+    isFooterStopped:false,
+  }, */
+ 
 ];
